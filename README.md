7d2c8ae5f4c1dbb22b7da6a1d68df4478db046fc
Puyan Wei <puyan.wei@mountainwarehouse.com>
2019-01-25 17:16:41 +0000

    Added parallax effect, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Checking out parallax effect
+
+With a little help from Kevin Powell's excellent [video](https://www.youtube.com/watch?v=SmolT-tV5Lw)
--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('imported into html file');
+$(window).scroll(function() {
+    parallax();
+});
+
+function parallax() {
+    let windowScroll = $(window).scrollTop();
+    $('.hero-container').css(
+        'background-position',
+        'center ' + windowScroll * 0.7 + 'px'
+    );
+}
